d8d2cf4304733c6abceb8538fe656c8477430c85
Vivien Guo <guov@student.unimelb.edu.au>
2024-06-15 00:38:21 +1000

    change router to accommodate github pages, fix setting styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { SettingsContext, useSettingsValues, GameContext, useGameValues } from "./utils";
 import { GamePage, LandingPage, SettingsPage } from './pages';
 import { StyleWrapper } from './components';
@@ -9,18 +9,25 @@ const App = () => {
   const settingsValues = useSettingsValues();
   const gameValues = useGameValues();
 
+  const router = createHashRouter(
+    [
+      {
+        path: "/",
+        children : [
+          { path: "/", element: <LandingPage /> },
+          { path: "settings", element: <SettingsPage /> },
+          { path: "game", element: <GamePage /> }
+        ]
+      }
+    ]
+  );
+
   return (
     <SettingsContext.Provider value={settingsValues}>
       <GameContext.Provider value={gameValues}>
         <StyleWrapper>
-          <BrowserRouter basename={process.env.PUBLIC_URL} > 
-            <Routes>
-              <Route index path="/" element={<LandingPage />} />
-              <Route path="/settings" element={<SettingsPage />} />
-              <Route path="/game" element={<GamePage />} />
-            </Routes>
-          </BrowserRouter>
-          </StyleWrapper>
+          <RouterProvider router={router} />
+        </StyleWrapper>
       </GameContext.Provider>
     </SettingsContext.Provider>
   );
